dd8c28e6d4232923f938a9264943784ab9b47b27
Matej Medved <matej.medved01@gmail.com>
2022-11-30 02:55:55 +0100

    Add texts,buttons, clicks

--- Main/main.py
+++ Main/main.py
@@ -6,26 +6,54 @@ from overworld import Overworld
 
 class Game:
     def __init__(self):
-        self.overworld = Overworld(surface=screen, level_method = self.create_level)
-        self.level = None
+        #Starting values
         self.max_level = 2
+        self.selected_level = 1
+
+        #Setup states
+        self.main_menu = None
+        self.options = None
+        self.level = None
+        self.overworld = Overworld(screen, self.selected_level, self.max_level, level_method = self.create_level, get_action = self.get_action)
         self.status = 'overworld'
+        
+        #Event data
+        self.clicks = []
 
     def create_level(self, level, surface):
+        self.selected_level = level
         self.level = Level(level,surface, self.create_overworld)
         self.status = 'level'
 
     def create_overworld(self, surface, current_level, max_level):
         if self.max_level < max_level:
             self.max_level = max_level
-        self.overworld = Overworld(surface, current_level, self.max_level, level_method = self.create_level)
+        self.overworld = Overworld(surface, current_level, self.max_level, level_method = self.create_level, get_action = self.get_action)
         self.status = 'overworld'
+    
+    def get_action(self, action):
+        return getattr(self, action)
+
+    def get_selected_level(self):
+        return self.overworld.selected_level
+
+    def open_options(self):
+        print("open options")
+
+    def to_main_menu(self):
+        print("to main menu")
+        #self.status = 'main_menu'
 
     def run(self):
-        if self.status == 'overworld':
-            self.overworld.run()
+        if self.status == 'main_menu':
+            self.main_menu.run(self.clicks)
+        elif self.status == 'options':
+            self.options.run(self.clicks)
+        elif self.status == 'overworld':
+            self.overworld.run(self.clicks)
         elif self.status == 'level':
             self.level.run()
+        self.clicks = []
 
 #Initialize pygame
 pygame.init()
@@ -39,6 +67,10 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                mouse_pos = pygame.mouse.get_pos()
+                game.clicks.append(mouse_pos)
     
     game.run()
 
--- Main/overworld.py
+++ Main/overworld.py
@@ -1,6 +1,41 @@
 import pygame
 from game_data import levels
 
+class Text(pygame.sprite.Sprite):
+    def __init__(self, position, text, size=30, color=(255, 255, 255)):
+        super().__init__()
+        self.font = pygame.font.SysFont('Arial', size)
+        self.text = self.font.render(text, True, color)
+        self.size = self.text.get_size()
+        self.image = pygame.Surface(self.size, pygame.SRCALPHA)
+        self.rect = self.image.get_rect(center=position)
+        self.image.blit(self.text, (0, 0))
+
+class Button(pygame.sprite.Sprite):
+    def __init__(self, position, image, text, action=None, get_action=None, *args):
+        super().__init__()
+
+        self.action = action
+        self.args = args
+        self.get_action = get_action
+        self.call_action = get_action(action)
+
+        self.font = pygame.font.SysFont('Arial', 30)
+        self.text = self.font.render(text, True, (255, 255, 255))
+        self.size = self.text.get_size()
+        self.image = pygame.Surface(self.size)
+        self.rect = self.image.get_rect(center=position)
+        self.image.fill((0, 0, 0))
+        self.image.blit(self.text, (0, 0))
+
+    def click(self):
+        if self.action:
+            if self.action == 'create_level':
+                selected_level = self.get_action('get_selected_level')()
+                self.call_action(selected_level, *self.args)
+            else:
+                self.call_action(*self.args)
+
 class Node(pygame.sprite.Sprite):
     def __init__(self, position, unlocked=False, speed=5, image=None):
         super().__init__()
@@ -25,7 +60,7 @@ class Node(pygame.sprite.Sprite):
                     self.pos = self.target
                     self.target = None
 class Overworld:
-    def __init__(self, surface, start_level=1, max_level=2, speed= 10, level_method=None):
+    def __init__(self, surface, start_level=1, max_level=2, speed= 10, level_method=None, get_action=None):
         self.surface = surface
         self.selected_level = start_level
         self.max_level = max_level
@@ -37,7 +72,16 @@ class Overworld:
         self.moving = False
 
         self.setup_nodes()
+        #Texts
+        self.texts = pygame.sprite.Group()
+        self.texts.add(Text((625, 100), 'LEVEL SELECTION', 70, (255, 255, 255)))
 
+        #Buttons
+        self.buttons = pygame.sprite.Group()
+        self.buttons.add(Button((625,625), None, "PLAY", "create_level", get_action, self.surface))
+        self.buttons.add(Button((625,675), None, "OPTIONS", "open_options", get_action))
+        self.buttons.add(Button((625,725), None, "BACK TO MAIN MENU", "to_main_menu", get_action))
+        
     def setup_nodes(self):
         self.nodes = pygame.sprite.Group()
 
@@ -47,9 +91,13 @@ class Overworld:
             else:  
                 self.nodes.add(Node((value['position'][0] - 425*(self.selected_level-1),value['position'][1]), speed=self.speed))
 
-    def input(self):
-        keys = pygame.key.get_pressed()
+    def input(self, clicks=None):
+        for click in clicks:
+            for button in self.buttons:
+                if button.rect.collidepoint(click):
+                    button.click()
 
+        keys = pygame.key.get_pressed()
         if not self.moving:
             if keys[pygame.K_RIGHT]:
                 self.moving = True
@@ -79,10 +127,12 @@ class Overworld:
                 running = True
         self.moving = running
 
-    def run(self):
-        self.input()
+    def run(self, clicks=None):
+        self.input(clicks)
         self.nodes.update()
         self.update()
 
         self.surface.fill((20, 20, 20))
         self.nodes.draw(self.surface)
+        self.buttons.draw(self.surface)
+        self.texts.draw(self.surface)
