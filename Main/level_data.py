e0cf55c1c796e48fc986f3f815961a16d90c4f38
Matej Medved <matej.medved01@gmail.com>
2023-03-15 23:23:08 +0100

    Levels

--- Main/level_data.py
+++ Main/level_data.py
@@ -49,47 +49,47 @@ level_5 = {
 }
 
 level_6 = {
-    'Terrain': './assets/levels/level_5/level_Terrain.csv',
-    'Decorations': './assets/levels/level_5/level_Decorations.csv',
-    'Collectables': './assets/levels/level_5/level_Collectables.csv',
-    'Player': './assets/levels/level_5/level_Player.csv',
-    'Enemy': './assets/levels/level_5/level_Enemies.csv',
-    'Constraints': './assets/levels/level_5/level_Constraints.csv',
+    'Terrain': './assets/levels/level_6/level_Terrain.csv',
+    'Decorations': './assets/levels/level_6/level_Decorations.csv',
+    'Collectables': './assets/levels/level_6/level_Collectables.csv',
+    'Player': './assets/levels/level_6/level_Player.csv',
+    'Enemy': './assets/levels/level_6/level_Enemies.csv',
+    'Constraints': './assets/levels/level_6/level_Constraints.csv',
     'background': ['./assets/background/background.png', './assets/background/background.png', './assets/background/background.png']
 }
 level_7 = {
-    'Terrain': './assets/levels/level_5/level_Terrain.csv',
-    'Decorations': './assets/levels/level_5/level_Decorations.csv',
-    'Collectables': './assets/levels/level_5/level_Collectables.csv',
-    'Player': './assets/levels/level_5/level_Player.csv',
-    'Enemy': './assets/levels/level_5/level_Enemies.csv',
-    'Constraints': './assets/levels/level_5/level_Constraints.csv',
+    'Terrain': './assets/levels/level_7/level_Terrain.csv',
+    'Decorations': './assets/levels/level_7/level_Decorations.csv',
+    'Collectables': './assets/levels/level_7/level_Collectables.csv',
+    'Player': './assets/levels/level_7/level_Player.csv',
+    'Enemy': './assets/levels/level_7/level_Enemies.csv',
+    'Constraints': './assets/levels/level_7/level_Constraints.csv',
     'background': ['./assets/background/background.png', './assets/background/background.png', './assets/background/background.png']
 }
 level_8 = {
-    'Terrain': './assets/levels/level_5/level_Terrain.csv',
-    'Decorations': './assets/levels/level_5/level_Decorations.csv',
-    'Collectables': './assets/levels/level_5/level_Collectables.csv',
-    'Player': './assets/levels/level_5/level_Player.csv',
-    'Enemy': './assets/levels/level_5/level_Enemies.csv',
-    'Constraints': './assets/levels/level_5/level_Constraints.csv',
+    'Terrain': './assets/levels/level_8/level_Terrain.csv',
+    'Decorations': './assets/levels/level_8/level_Decorations.csv',
+    'Collectables': './assets/levels/level_8/level_Collectables.csv',
+    'Player': './assets/levels/level_8/level_Player.csv',
+    'Enemy': './assets/levels/level_8/level_Enemies.csv',
+    'Constraints': './assets/levels/level_8/level_Constraints.csv',
     'background': ['./assets/background/background.png', './assets/background/background.png', './assets/background/background.png']
 }
 level_9 = {
-    'Terrain': './assets/levels/level_5/level_Terrain.csv',
-    'Decorations': './assets/levels/level_5/level_Decorations.csv',
-    'Collectables': './assets/levels/level_5/level_Collectables.csv',
-    'Player': './assets/levels/level_5/level_Player.csv',
-    'Enemy': './assets/levels/level_5/level_Enemies.csv',
-    'Constraints': './assets/levels/level_5/level_Constraints.csv',
+    'Terrain': './assets/levels/level_9/level_Terrain.csv',
+    'Decorations': './assets/levels/level_9/level_Decorations.csv',
+    'Collectables': './assets/levels/level_9/level_Collectables.csv',
+    'Player': './assets/levels/level_9/level_Player.csv',
+    'Enemy': './assets/levels/level_9/level_Enemies.csv',
+    'Constraints': './assets/levels/level_9/level_Constraints.csv',
     'background': ['./assets/background/background.png', './assets/background/background.png', './assets/background/background.png']
 }
 level_10 = {
-    'Terrain': './assets/levels/level_5/level_Terrain.csv',
-    'Decorations': './assets/levels/level_5/level_Decorations.csv',
-    'Collectables': './assets/levels/level_5/level_Collectables.csv',
-    'Player': './assets/levels/level_5/level_Player.csv',
-    'Enemy': './assets/levels/level_5/level_Enemies.csv',
-    'Constraints': './assets/levels/level_5/level_Constraints.csv',
+    'Terrain': './assets/levels/level_10/level_Terrain.csv',
+    'Decorations': './assets/levels/level_10/level_Decorations.csv',
+    'Collectables': './assets/levels/level_10/level_Collectables.csv',
+    'Player': './assets/levels/level_10/level_Player.csv',
+    'Enemy': './assets/levels/level_10/level_Enemies.csv',
+    'Constraints': './assets/levels/level_10/level_Constraints.csv',
     'background': ['./assets/background/background.png', './assets/background/background.png', './assets/background/background.png']
 }
